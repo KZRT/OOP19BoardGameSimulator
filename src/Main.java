e5e57106214034fb8faa113f1348f84885b7681f
KZRT <kzrt0123@gmail.com>
2019-11-29 18:31:16 +0900

    Poker Is Done. HandScorer is for evaluate player & dealer's hand Change Game Interface for more convinient development

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Dice a = new Dice();
-        for (int i = 0; i < 100; i++) {
-
-            a.roll();
-            a.print();
+        Poker a = new Poker();
+        int card = 0;
+        System.out.println("How much will you bet: ");
+        Scanner input = new Scanner(System.in);
+        int bet = input.nextInt();
+        input.nextLine();
+        a.initialize(bet);
+        while(!a.isGameEnd())
+        {
+            System.out.flush();
+            if(!a.printOneTurn()) break;
+            if(!a.nextTurn(input)) break;
         }
+        card += a.cheapGain(bet);
+        System.out.print(card);
     }
 }
