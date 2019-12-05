714df62b5e35018d775441d0268c2ca91e486282
Jongwoo Na <njw1204@naver.com>
2019-12-05 20:41:52 +0900

    Add baccarat to game manager

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,12 +4,12 @@ public class Main {
     public static void main(String[] args) {
         GameManager gm = new GameManager();
         int gameNum = 0;
-    	do {
-    		System.out.print("Which game to play? 1.Poker 2.BlackJack 0.Finish\n");
+        do {
+            System.out.print("Which game to play? 1.Poker 2.BlackJack 3.Baccarat 0.Finish\n");
             Scanner input = new Scanner(System.in);
             gameNum = input.nextInt();
             gm.gametoPlay(gameNum);
-    	} while (gameNum != 0);
-        
+        } while (gameNum != 0);
+
     }
 }
