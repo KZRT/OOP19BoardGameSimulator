72e44ac91f5008d1cd52c60faa438cb5488a3df1
KZRT <kzrt0123@gmail.com>
2019-12-06 21:11:45 +0900

    Finalize Player

--- src/Chip.java
+++ src/Chip.java
@@ -1,15 +1,37 @@
 public enum Chip {
 
-    WHITE(1), YELLOW(2), RED(5), BLUE(10), GREY(20), GREEN(25), ORANGE(50),
+    WHITE(1), YELLOW( 2), RED(5), BLUE(10), GREY(20), GREEN(25), ORANGE(50),
     BLACK(100), PINK(250), PURPLE(500), GOLD(1000), SKY(2000), BROWN(5000);
 
     private final int chipValue;
 
-    Chip(int price) {
-        this.chipValue = price;
+
+    Chip(int value) {
+        this.chipValue = value;
     }
 
     public int getChipValue() {
         return this.chipValue;
+    }
+
+    public static int indexToChipValue(int index)
+    {
+        switch(index)
+        {
+            case 0: return WHITE.chipValue;
+            case 1: return YELLOW.chipValue;
+            case 2: return RED.chipValue;
+            case 3: return BLUE.chipValue;
+            case 4: return GREY.chipValue;
+            case 5: return GREEN.chipValue;
+            case 6: return ORANGE.chipValue;
+            case 7: return BLACK.chipValue;
+            case 8: return PINK.chipValue;
+            case 9: return PURPLE.chipValue;
+            case 10: return GOLD.chipValue;
+            case 11: return SKY.chipValue;
+            case 12: return BROWN.chipValue;
+            default: return 0;
+        }
     }
 }
--- src/Player.java
+++ src/Player.java
@@ -3,37 +3,60 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 
+
 public class Player {
     private int wallet;
     private int chipTotal;
-    private ArrayList<Chip> chips;
+    private int[] chipCount;
 
     Player() {
         wallet = 0;
         chipTotal = 0;
-        chips = new ArrayList<>();
+        chipCount = new int[13];
     }
 
     Player(int amount) {
         wallet = amount;
         chipTotal = 0;
-        chips = new ArrayList<>();
+        chipCount = new int[13];
     }
 
-    public int getWallet() {return this.wallet;}
-    public void printWallet()
-    {
+    public int getWallet() {
+        return this.wallet;
+    }
+
+    public void printWallet() {
         System.out.println("Player currently has " + wallet + "$");
         System.out.println("Player currently has " + chipTotal + "chips");
     }
-    public int chipToWallet(int amount)
-    {
-        for(Chip chip: chips)
-        {
-            wallet += chip.getChipValue();
+
+    public int chipToWallet() {
+        for (int i = 0; i < 13; i++) {
+            wallet += Chip.indexToChipValue(i) * chipCount[i];
+            chipCount[i] = 0;
         }
+        chipTotal = 0;
         return wallet;
     }
-    ///fuckfucksafdhadsugids
 
+    public int specificChipToWallet(int index) {
+        wallet += Chip.indexToChipValue(index) * chipCount[index];
+        chipTotal -= Chip.indexToChipValue(index) * chipCount[index];
+        chipCount[index] = 0;
+        return wallet;
+    }
+
+    public int walletToChip(int[] chipCounts) throws SecurityException{
+        for(int i=12; i>=0; i++)
+        {
+            if(wallet - chipCounts[i] * Chip.indexToChipValue(i) < 0)
+            {
+                throw new SecurityException();
+            }
+            wallet -= chipCounts[i] * Chip.indexToChipValue(i);
+            chipCount[i] += chipCounts[i];
+            chipTotal += chipCounts[i] * Chip.indexToChipValue(i);
+        }
+        return chipTotal;
+    }
 }
